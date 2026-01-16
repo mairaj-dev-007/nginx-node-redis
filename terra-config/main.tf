d2e66751f2a6921f3be00a221f85496da9919d88
mairaj-dev-007 <mairaj.imk.dev@gmail.com>
2026-01-16 15:31:02 +0500

    change in user_data

--- terra-config/main.tf
+++ terra-config/main.tf
@@ -3,6 +3,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+variable "github_pat" {
+  description = "GitHub Personal Access Token for cloning private repo"
+  type        = string
+  sensitive   = true
+}
 
 resource "aws_security_group" "web_sg" {
   name        = "nginx-web-sg"
@@ -80,7 +85,8 @@ resource "aws_instance" "web" {
               rm -rf /home/ubuntu/nginx-node-redis
               
               # Clone into the ubuntu user's directory
-              git clone https://github.com/mairaj-dev-007/nginx-node-redis.git
+              git clone https://${var.github_pat}@github.com/mairaj-dev-007/nginx-node-redis.git
+
               
               echo "--- Launching App ---"
               cd /home/ubuntu/nginx-node-redis
