76038f3839a5b7a53eceb62571521b909936018c
mairaj-dev-007 <mairaj.imk.dev@gmail.com>
2026-01-16 17:15:22 +0500

    change in user_data

--- terra-config/main.tf
+++ terra-config/main.tf
@@ -54,7 +54,8 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+
   }
 
   filter {
